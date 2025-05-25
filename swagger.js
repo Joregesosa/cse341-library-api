d72f0e416ff4f3fe090c7c21f159c2b12f67b2ba
Jorge Sosa <joregesosa@gmail.com>
2025-05-24 22:24:12 -0400

    refactor: simplify host configuration in swagger.js

--- swagger.js
+++ swagger.js
@@ -1,12 +1,17 @@
 const swaggerAutogen = require('swagger-autogen')();
 const appConfig = require('./config');
 
+const host =
+  appConfig.env === 'production'
+    ? appConfig.host
+    : appConfig.host + ':' + appConfig.port;
+
 const doc = {
   info: {
     title: 'Contacts API',
     description: 'API for managing contacts cse341 first project',
   },
-  host: appConfig.host + ':' + appConfig.port,
+  host: host,
   basePath: '/api/v1',
   schemes: ['http', 'https'],
 };
