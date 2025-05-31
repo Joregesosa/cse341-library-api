65af2c1d243c0ca76a369a10a76ff17fdb744162
Jorge Sosa <joregesosa@gmail.com>
2025-05-31 17:43:01 -0400

    refactor: streamline Book model schema by removing redundant timestamp fields

--- models/Book.js
+++ models/Book.js
@@ -1,36 +1,31 @@
 // mongoos model for book
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    coverUrl: {
+      type: String,
+      required: false,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    publishedYear: {
+      type: Number,
+      required: false,
+    },
   },
-  author: {
-    type: String,
-    required: true,
-  },
-  coverUrl: {
-    type: String,
-    required: false,
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  publishedYear: {
-    type: Number,
-    required: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true },
+);
 
 const Book = mongoose.model('Book', bookSchema);
 
