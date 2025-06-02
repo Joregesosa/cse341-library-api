2bcfb23c67caf74f645d6eec3d0d0ffae366fcdb
Jorge Sosa <joregesosa@gmail.com>
2025-06-02 19:17:01 -0400

    feat: configure CORS and initialize passport middleware in server setup

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = require('./routes');
 const dbConn = require('./config/db');
 const appConfig = require('./config');
+const passport = require('./config/passport');
+const cors = require('cors');
 const routeNotFoundMiddleware = require('./middlewares/routeNotFound.middleware');
 const {
   joiErrorMiddleware,
@@ -13,9 +15,25 @@ dbConn(); // Connect to the database
 
 const app = express();
 
-app.use(express.json());
+// Configurar CORS antes de cualquier middleware de ruta
+app
+  .use(express.json())
+  .use(require('./middlewares/express-session.middleware'))
+  .use(passport.initialize())
+  .use(passport.session())
+  .use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Permitir todas las solicitudes CORS
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    next();
+  })
+  .use(cors({ methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] }))
+  .use(cors({ origin: '*' }));
 
-// Rutas
+//app.use(express.urlencoded({ extended: true }));
+
 router(app);
 
 // Middleware para capturar rutas no existentes
