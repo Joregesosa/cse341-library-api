f497bff50cb159a1218bf16d8223a8e9bb7f3a7a
Jorge Sosa <joregesosa@gmail.com>
2025-06-02 19:17:47 -0400

    feat: refactor GitHub authentication routes and remove unused endpoints

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,20 +1,27 @@
-const {
-  index,
-  show,
-  create,
-  update,
-  destroy,
-} = require('../controllers/auth.controller');
-const {
-  validateUser,
-  validateUserUpdate,
-} = require('../validators/auth.schema');
+const { isAuthenticated } = require('../middlewares/auth.middleware');
 const authRoutes = require('express').Router();
+const {
+  show,
+  update,
+  githubLogin,
+  githubCallback,
+  logout,
+} = require('../controllers/auth.controller');
+const { validateUserUpdate } = require('../validators/auth.schema');
 
-authRoutes.get('/', index);
-authRoutes.get('/:id', show);
-authRoutes.post('/', validateUser, create);
-authRoutes.put('/:id', validateUserUpdate, update);
-authRoutes.delete('/:id', destroy);
+authRoutes.get(
+  '/github/login',
+  githubLogin /* 
+  #swagger.tags = ['Auth']
+    #swagger.summary = 'Redirect to GitHub for authentication'
+    #swagger.description = "Initiates GitHub OAuth authentication process. It can't be tested directly in Swagger UI, but you can use Postman or a browser to test it."
+    #swagger.deprecated = true
+    */,
+);
+authRoutes.get('/github/callback', githubCallback /* #swagger.ignore = true */);
+authRoutes.get('/github/logout', isAuthenticated, logout);
+
+authRoutes.get('/profile', isAuthenticated, show);
+authRoutes.put('/profile', isAuthenticated, validateUserUpdate, update);
 
 module.exports = authRoutes;
