7b43d10e7f0a05705d2f43ef8f0d8a9b0b66c994
Jorge Sosa <joregesosa@gmail.com>
2025-06-02 19:18:10 -0400

    feat: update routing to enforce authentication and profile completion for book routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
+const {
+  isAuthenticated,
+  isProfileComplete,
+} = require('../middlewares/auth.middleware');
 
 const appRouter = Router();
 
 function router(app) {
-  app.use('/api/v1', appRouter);
+  app.use('/', appRouter);
 
   appRouter.use(
     '/api-docs',
@@ -11,7 +15,12 @@ function router(app) {
     /* #swagger.ignore = true */
   );
   appRouter.use('/auth', require('./auth.routes'));
-  appRouter.use('/books', require('./book.routes'));
+  appRouter.use(
+    '/books',
+    isAuthenticated,
+    isProfileComplete,
+    require('./book.routes'),
+  );
 }
 
 module.exports = router;
