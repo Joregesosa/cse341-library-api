bf806beb3e2cccde286379215a234afe7db7a212
Jorge Sosa <joregesosa@gmail.com>
2025-06-02 21:15:27 -0400

    fix: update session cookie domain configuration for production environment

--- middlewares/express-session.middleware.js
+++ middlewares/express-session.middleware.js
@@ -10,7 +10,10 @@ const sessionMiddleware = session({
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: appConfig.env === 'production' ? 'none' : 'lax', // 'none' solo en producción
-    domain: appConfig.env === 'production' ? '.onrender.com' : undefined, // Ajusta según tu dominio
+    domain:
+      appConfig.env === 'production'
+        ? 'https://cse341-library-api-wlf4.onrender.com'
+        : undefined, // Ajusta según tu dominio
   },
   proxy: appConfig.env === 'production', // Importante si estás detrás de un proxy
 });
